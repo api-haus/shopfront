35919ce5eec5559c85b0e3cd0379b35bc36838c3
Yurii Chyrkov <mail@api.haus>
2024-07-05 17:37:34 +0300

    feat: header in adv spend thingy

--- src/api/endpoints/wb/adv-spend.ts
+++ src/api/endpoints/wb/adv-spend.ts
@@ -1,13 +1,19 @@
 import { FastifyInstance } from 'fastify';
 
-export default async function (app: FastifyInstance) {
+export default function (app: FastifyInstance) {
   app.route({
     method: 'GET',
     url: '/adv-spend',
     handler: () => {
       return [
-        1234566,
-        212,
+        [
+          'SKU',
+          'Sum Adv Spend',
+        ],
+        [
+          1234566,
+          212,
+        ],
       ];
     },
   });
